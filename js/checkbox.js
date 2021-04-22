1bf016b1c5cea802124128ebaacd7e618cb3785f
Lisov_Nikita <niklis60003@gmail.com>
2021-04-22 04:26:25 +0300

    create_new_application_page2

--- js/checkbox.js
+++ js/checkbox.js
@@ -2,6 +2,7 @@ let page_type = document.getElementById('type_page').getAttribute('data-value');
 let first_input;
 let second_input;
 let count = 0;
+let radio = [0,0,0,0];
 function Check(e) {
   let labels = document.querySelectorAll('label')
   if (page_type == 'create_application') {
@@ -34,22 +35,96 @@ function Check(e) {
         }
       }
 
-  }else{
-    // if (e.checked == true) {
-    //   for (var i = 0; i < labels.length; i++) {
-    //     if (labels[i].getAttribute('for') == e.getAttribute('id')) {
-    //       labels[i].style.color = "#525A68"
-    //       count ++;
-    //     }
-    //   }
-    // }
-    // if (e.checked == false) {
-    //   for (var i = 0; i < labels.length; i++) {
-    //     if (labels[i].getAttribute('for') == e.getAttribute('id')) {
-    //       labels[i].style.color = "#BEC0C2"
-    //       count --;
-    //     }
-    //   }
-    // }
   }
+  if (page_type == 'radio'){
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].style.color = "#BEC0C2"
+        for (var j = 1; j < radio.length; j++) {
+          labels[radio[j]].style.color = "#525A68"
+          console.log(radio[j]);
+        }
+    }
+    if (e.checked == true) {
+      for (var i = 0; i < labels.length; i++) {
+        if (labels[i].getAttribute('for') == e.getAttribute('id')) {
+          labels[i].style.color = "#525A68"
+          radio[0]=i;
+        }
+      }
+    }
+  }
+}
+
+function Check2(e) {
+  let labels = document.querySelectorAll('label')
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].style.color = "#BEC0C2"
+        for (var j = 0; j < radio.length; j++) {
+          labels[radio[j]].style.color = "#525A68"
+          console.log(radio[j]);
+        }
+
+    }
+    if (e.checked == true) {
+      for (var i = 0; i < labels.length; i++) {
+        if (labels[i].getAttribute('for') == e.getAttribute('id')) {
+          labels[i].style.color = "#525A68"
+          radio[1]=i;
+        }
+      }
+    }
+}
+function Check3(e) {
+  let labels = document.querySelectorAll('label')
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].style.color = "#BEC0C2"
+        for (var j = 0; j < radio.length; j++) {
+          labels[radio[j]].style.color = "#525A68"
+          console.log(radio[j]);
+        }
+    }
+    if (e.checked == true) {
+      for (var i = 0; i < labels.length; i++) {
+        if (labels[i].getAttribute('for') == e.getAttribute('id')) {
+          labels[i].style.color = "#525A68"
+          radio[2]=i;
+        }
+      }
+    }
+}
+function Check4(e) {
+  let labels = document.querySelectorAll('label')
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].style.color = "#BEC0C2"
+        for (var j = 0; j < radio.length; j++) {
+          labels[radio[j]].style.color = "#525A68"
+          console.log(radio[j]);
+        }
+    }
+    if (e.checked == true) {
+      for (var i = 0; i < labels.length; i++) {
+        if (labels[i].getAttribute('for') == e.getAttribute('id')) {
+          labels[i].style.color = "#525A68"
+          radio[3]=i;
+        }
+      }
+    }
+}
+function Check5(e) {
+  let labels = document.querySelectorAll('label')
+    for (var i = 0; i < labels.length; i++) {
+        labels[i].style.color = "#BEC0C2"
+        for (var j = 0; j < radio.length; j++) {
+          labels[radio[j]].style.color = "#525A68"
+          console.log(radio[j]);
+        }
+    }
+    if (e.checked == true) {
+      for (var i = 0; i < labels.length; i++) {
+        if (labels[i].getAttribute('for') == e.getAttribute('id')) {
+          labels[i].style.color = "#525A68"
+          radio[4]=i;
+        }
+      }
+    }
 }
