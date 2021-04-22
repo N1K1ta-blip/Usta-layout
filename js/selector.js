61febd9d6b040c0e7abc837be3000c42d24e2653
Lisov_Nikita <niklis60003@gmail.com>
2021-04-22 22:28:28 +0300

    fix_page4.1

--- js/selector.js
+++ js/selector.js
@@ -5,6 +5,8 @@ const optionsContainer2 = document.querySelector(".options-container2");
 let select_count1 = 0;
 let select_count2 = 0;
 let select_count3 = 0;
+let select_count4 = 0;
+let select_count5 = 0;
 const optionsList = document.querySelectorAll(".option");
 
 selected.addEventListener("click", () => {
@@ -93,5 +95,36 @@ function OtherValue(e) {
     document.getElementById('current_value2').innerHTML = e.innerHTML
     document.getElementsByClassName('additionally_block')[2].style.display = "none"
     select_count3 = 0
+  }
+  if(value == 4){
+    document.getElementById('current_value4').innerHTML = e.innerHTML
+    document.getElementById('first_options').style.display = "none"
+    select_count4 = 0
+  }
+  if(value == 5){
+    document.getElementById('current_value5').innerHTML = e.innerHTML
+    document.getElementById('second_options').style.display = "none"
+    select_count5 = 0
+  }
+}
+function Search_number(e) {
+  let value = e.getAttribute('data-value')
+  if(value == 0){
+    if (select_count4 == 0) {
+      document.getElementById('first_options').style.display = 'block'
+      select_count4 ++
+    }else{
+      document.getElementById('first_options').style.display = 'none'
+      select_count4 --
+    }
+  }
+  if(value == 1){
+    if (select_count5 == 0) {
+      document.getElementById('second_options').style.display = 'block'
+      select_count5 ++
+    }else{
+      document.getElementById('second_options').style.display = 'none'
+      select_count5 --
+    }
   }
 }
